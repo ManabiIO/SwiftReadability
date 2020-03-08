80fcd12370122e68a2612af1568e7bb46c36e1ff
Alex Ehlke <alex.ehlke@gmail.com>
2020-03-08 12:29:09 -0400

    update readability.js to latest

--- SwiftReadabilityExample/ViewController.swift
+++ SwiftReadabilityExample/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
 //    let articleURL = URL(string: "http://www.huffingtonpost.jp/2017/07/01/earthquake-kumamoto0702_n_17358712.html?utm_hp_ref=japan&ir=Japan")
 //    let articleURL = URL(string: "http://www.ichitetsu.com/2017/08/84-064b.html")
 //    let articleURL = URL(string: "https://www.cnn.co.jp/m/usa/35115947.html")
-    let articleURL = URL(string: "https://matcha-jp.com/easy/1064")
+    let articleURL = URL(string: "https://hypebeast.com/jp/2020/2/doublet-marionette-pattern-clothes")
     //let articleURL = URL(string: "https://github.com/blog/2195-the-shape-of-open-source")
     
     
