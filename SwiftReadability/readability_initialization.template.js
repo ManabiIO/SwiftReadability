3d3f10f5b0263b68db4dae7a369305e36b877609
Alex Ehlke <alex.ehlke@gmail.com>
2020-03-10 20:27:51 -0400

    save classes as firefox does

--- SwiftReadability/readability_initialization.template.js
+++ SwiftReadability/readability_initialization.template.js
@@ -9,6 +9,19 @@ var uri = {
     pathBase: loc.protocol + "//" + loc.host + loc.pathname.substr(0, loc.pathname.lastIndexOf("/") + 1)
 };
 var article = new Readability(uri, document, {
+    // https://github.com/mozilla/gecko-dev/blob/246928d59c6c11e1c3b3b0a6b00534bfc075e3c4/toolkit/components/reader/ReaderMode.jsm#L21-L31
+    classesToPreserve: [
+        "caption",
+        "emoji",
+        "hidden",
+        "invisible",
+        "sr-only",
+        "visually-hidden",
+        "visuallyhidden",
+        "wp-caption",
+        "wp-caption-text",
+        "wp-smiley"
+    ],
     charThreshold: ##CHAR_THRESHOLD##
 }).parse();
 
